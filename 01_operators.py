ccd3191c7a3de958b20ea9d0d441d7bcb91383f7
SharjeelAhmad12 <fa20-bst-011@cuilahore.edu.pk>
2023-02-04 14:46:00 +0500

    My first commit

---
+++ 01_operators.py
@@ -0,0 +1,14 @@
+#there are three type of operators
+print(2+1)
+print(3-1)
+print(6/2)
+print(2*3)
+print(13%2)
+print(6//2)
+print(2**3)
+print(3**2/2*3/3+6-4)
+#what is PEMDAS
+#parenthesis,exponents,multiply,divide,addition,substraction
+#() this is parenthesis jo be parenthesis ma ho ga sab se phale wo  solve ho ga
+
+
